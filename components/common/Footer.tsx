c562e93838251391adabcd8c1d83e4218fac0fed
tedlee <ted.lee@bclabs.co.kr>
2023-08-16 23:30:41 +0900

    add: footer component added

---
+++ components/common/Footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+import PlayerContainer from "@/components/player/PlayerContainer";
+
+const Footer = () => {
+  return (
+    <>
+      <PlayerContainer />
+    </>
+  );
+};
+
+export default Footer;
