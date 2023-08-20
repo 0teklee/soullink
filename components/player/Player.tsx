d934d1ba3dd432edd18490107e2a225dfea5f985
tedlee <ted.lee@bclabs.co.kr>
2023-08-20 21:56:30 +0900

    fix: applied new list item type

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -1,31 +1,34 @@
 "use client";
 
 import React, { Dispatch, RefObject, SetStateAction } from "react";
-import ReactPlayer from "react-player";
 import { PlayerProps, PlayerState } from "@/types/common/playerTypes";
 import { secondsFormatter } from "@/utils/client/formatter";
+import ReactPlayer from "react-player";
 
-const Player = (props: {
+const Player = ({
+  playerState,
+  setPlayerState,
+  playerRef,
+  song,
+}: {
   playerState: PlayerState;
   setPlayerState: Dispatch<SetStateAction<PlayerState>>;
   playerRef: RefObject<PlayerProps>;
+  song: string;
 }) => {
-  const { playerState, setPlayerState, playerRef } = props;
-
   const { playing, played, duration, seeking, volume, muted } = playerState;
-
-  const playerCur = playerRef?.current;
 
   return (
     <>
       <ReactPlayer
         //@ts-ignore
         ref={playerRef}
-        className={``}
-        url={`https://soundcloud.com/gum_mp3/in-my-room-utada-hikaru-db-garage-mix?si=7329048ff1f8437cb3f775d34b745e38&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing`}
+        className={`hidden`}
+        url={song}
         playing={playing}
         volume={volume}
         muted={muted}
+        pip={true}
         onReady={(ReactPlayer) => {
           setPlayerState({
             ...playerState,
