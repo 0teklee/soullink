57553fba05592b217dde59f4e572999ae8457631
tedlee <ted.lee@bclabs.co.kr>
2023-08-25 15:27:51 +0900

    fix: list menu item key added

--- components/player/module/ListMenuContainer.tsx
+++ components/player/module/ListMenuContainer.tsx
@@ -30,6 +30,7 @@ const ListMenuContainer = ({
             {...song}
             setCurIndex={setCurIndex}
             curIndex={curIndex}
+            index={index}
           />
         ))}
       </div>
