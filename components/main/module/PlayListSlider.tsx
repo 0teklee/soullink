d156ba6a05f47901bff5f35939753366879f3f84
tedlee <ted.lee@bclabs.co.kr>
2023-08-25 15:27:26 +0900

    add : playlist slider added

---
+++ components/main/module/PlayListSlider.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import PlaylistItem from "@/components/main/module/PlaylistItem";
+
+import "swiper/css";
+import { PlaylistType } from "@/types/common/PlaylistType";
+
+const PlayListSlider = ({ playLists }: { playLists: PlaylistType[] }) => {
+  return (
+    <>
+      <div className={`w-full`}>
+        <Swiper
+          className={`overflow-fadeout`}
+          wrapperClass={`w-full`}
+          setWrapperSize={false}
+          breakpoints={{
+            "400": { slidesPerView: 1, spaceBetween: 0, width: 320 },
+            "768": { slidesPerView: 3, spaceBetween: 50 },
+            "1300": {
+              slidesPerView: 3,
+              spaceBetween: 300,
+            },
+            "1500": { slidesPerView: 2, spaceBetween: 200 },
+          }}
+        >
+          {playLists.map((item, index) => {
+            return (
+              <SwiperSlide key={`slide_item_${item.id}_${index}`}>
+                <PlaylistItem playlistItem={item} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default PlayListSlider;
