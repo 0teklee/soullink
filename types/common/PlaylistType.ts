0e78aa856c658d06ce36f9cf74047f67e549c93f
tedlee <ted.lee@bclabs.co.kr>
2023-08-26 22:47:36 +0900

    refactor: added playlist, song, player type in one file

--- types/common/PlaylistType.ts
+++ types/common/PlaylistType.ts
@@ -1,3 +1,6 @@
+import { ReactPlayerProps } from "react-player";
+import { CommentType, UserType } from "@/types/common/userType";
+
 export interface SongType {
   id: string;
   artist: string;
@@ -10,10 +13,43 @@ export interface SongType {
 export interface PlaylistType {
   id: string;
   title: string;
-  description: string;
   coverImage: string;
+  description: string;
+  contentLink?: string;
+  author: UserType;
+  authorId: string;
   songs: SongType[];
-  author: string;
+  comments: CommentType[];
+  likedBy: UserType[];
+  playCount: number;
   createdAt: string;
   updatedAt: string;
+}
+
+export interface PlayerState {
+  playing: boolean;
+  played: string;
+  duration: string;
+  playedSec: number;
+  durationSec: number;
+  volume: number;
+  muted: boolean;
+  seeking: boolean;
+  isLoading: boolean;
+}
+
+export interface PlayerListItem extends SongType {}
+
+export interface PlayerProps extends ReactPlayerProps {
+  seekTo(amount: number, type?: "seconds" | "fraction"): void;
+
+  getCurrentTime(): number;
+
+  getSecondsLoaded(): number;
+
+  getDuration(): number;
+
+  getInternalPlayer(key?: string): Record<string, any>;
+
+  showPreview(): void;
 }
