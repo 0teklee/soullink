00b5940ab70d278c530c15ea6dd7cc16ce2ee4ec
tedlee <ted.lee@bclabs.co.kr>
2023-08-30 21:37:57 +0900

    add: added page route for create playlist

---
+++ app/playlist/create/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlaylistCreateTemplate from "@/components/playlistCreate/PlaylistCreateTemplate";
+
+const Page = () => {
+  const a = 1;
+  return (
+    <>
+      <PlaylistCreateTemplate />
+    </>
+  );
+};
+
+export default Page;
