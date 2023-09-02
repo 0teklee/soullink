ff5d193ff6ddbb98295b177eec8d68e5308410fc
tedlee <ted.lee@bclabs.co.kr>
2023-09-02 16:01:39 +0900

    add: playlist create page

--- app/playlist/create/page.tsx
+++ app/playlist/create/page.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import PlaylistCreateTemplate from "@/components/playlistCreate/PlaylistCreateTemplate";
 
 const Page = () => {
-  const a = 1;
-  return (
-    <>
-      <PlaylistCreateTemplate />
-    </>
-  );
+  return <PlaylistCreateTemplate />;
 };
 
 export default Page;
