594fde6e67adca00da021c3f8a83f3fd0bbc7d91
tedlee <ted.lee@bclabs.co.kr>
2023-09-03 23:26:36 +0900

    add: Playlist create submit module

---
+++ components/playlistCreate/PlaylistCreateSubmit.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { CreatePlaylistType } from "@/types/common/Song&PlaylistType";
+import { useRouter } from "next/navigation";
+import { useMutation } from "react-query";
+
+const PlaylistCreateSubmit = ({
+  payload,
+  isPayloadValid,
+}: {
+  payload: CreatePlaylistType;
+  isPayloadValid: boolean;
+}) => {
+  const { title, description, songs } = payload;
+
+  const router = useRouter();
+
+  const fetcherCreatePlaylist = async () => {
+    const res = await fetch(`/api/playlist/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...payload,
+        userId: "966cd2cd-3cad-4e95-8ff1-451bad286fdf",
+      }),
+    });
+    const data = await res.json();
+    return data;
+  };
+
+  const { mutate } = useMutation(fetcherCreatePlaylist, {
+    onSuccess: (data) => {
+      router.push(`/playlist/${data.playlistTitle.replace(" ", "-")}`);
+    },
+  });
+
+  return (
+    <div>
+      <button
+        className={`px-5 py-2 rounded-xl text-white text-lg font-medium bg-primary disabled:bg-gray-300`}
+        disabled={isPayloadValid ? false : true}
+        onClick={() => {
+          mutate();
+        }}
+      >
+        submit
+      </button>
+    </div>
+  );
+};
+
+export default PlaylistCreateSubmit;
