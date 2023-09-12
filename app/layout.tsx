f42100a4921276492c22dedb7d4b7a6ab1819b48
tedlee <ted.lee@bclabs.co.kr>
2023-09-13 00:21:31 +0900

    fix: added next-auth session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
-import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import NextAuthSessionProvider from "@/components/common/module/NextAuthSessionProvider";
+import Header from "@/components/common/header/Header";
+import { getToken } from "next-auth/jwt";
 
 export const metadata = {
   title: "soullink",
@@ -15,13 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Header />
-        <main
-          className={`xs:my-10 py-12 xs:py-2 xs:px-4 xl:px-24 3xl:px-48 desktop:px-[400px] bg-white `}
-        >
-          {children}
-        </main>
-        <Footer />
+        <NextAuthSessionProvider>
+          <Header />
+          <main
+            className={`xs:my-10 py-12 xs:py-2 xs:px-4 xl:px-24 3xl:px-48 desktop:px-[400px] bg-white `}
+          >
+            {children}
+          </main>
+          <Footer />
+        </NextAuthSessionProvider>
       </body>
     </html>
   );
