1beb19b6a3f8a4928007d9f863aa66d821f3de00
tedlee <ted.lee@bclabs.co.kr>
2023-09-14 21:56:49 +0900

    feat: connected api request

--- components/signup/SignupTemplate.tsx
+++ components/signup/SignupTemplate.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Title from "@/components/common/module/Title";
 import { handleImageUpload } from "@/libs/utils/client/ImageUpload";
 import Image from "next/image";
+import { useMutation } from "react-query";
+import { SignupPayload } from "@/types/common/userType";
+import { useSession } from "next-auth/react";
+import { router } from "next/client";
 
 const SignupTemplate = () => {
-  const [payload, setPayload] = useState({
+  const { data: session, status } = useSession();
+  const userEmail = session?.user?.email;
+  const [payload, setPayload] = useState<SignupPayload>({
     profilePic: "",
     nickname: "",
-    description: "",
+    bio: "",
+    email: userEmail || "",
     socialLinks: {
       website: "",
       instagram: "",
@@ -17,18 +24,53 @@ const SignupTemplate = () => {
     },
   });
 
-  const isSubmitDisabled =
-    payload.nickname === "" && payload.description === "";
+  const isOAuthSignIn = status === "authenticated";
+
+  const isSubmitDisabled = payload.nickname === "" && payload.bio === "";
+
+  const fetcherSignup = async (reqPayload: SignupPayload) => {
+    const data = await fetch(`/api/user/signup`, {
+      method: `POST`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqPayload),
+    }).then((res) => res.json());
+    return data;
+  };
+
+  const { mutate } = useMutation(fetcherSignup, {
+    onSuccess: (data) => {
+      // modal 추가;
+      router.push(`/user/${payload.nickname}`);
+    },
+  });
+
+  const handleSignup = (arrPayload: SignupPayload) => {
+    mutate(arrPayload);
+  };
 
   const handlePayloadImgUpload = (imgUrl: string) => {
     setPayload((prev) => ({
       ...prev,
-      coverImage: imgUrl,
+      profilePic: imgUrl,
     }));
   };
 
+  console.log(`session`, session);
+
+  useEffect(() => {
+    setPayload((prev) => ({
+      ...prev,
+      email: userEmail || "",
+    }));
+    console.log("session", session);
+  }, [session]);
+
   return (
-    <section className={`flex flex-col items-center gap-12 py-6 xl:px-24`}>
+    <section
+      className={`flex flex-col items-center gap-12 py-6 xl:px-24 xs:px-3`}
+    >
       <Title size={`h1`} text={`Sign up`} />
       <div className={`flex items-center gap-12 w-full xs:flex-col`}>
         <div className={`relative flex flex-col items-center gap-1`}>
@@ -88,20 +130,20 @@ const SignupTemplate = () => {
         </div>
       </div>
       <div className={`flex flex-col items-start gap-2 w-full`}>
-        <Title size={`h2`} text={`Description`} />
+        <Title size={`h2`} text={`Bio`} />
         <textarea
           className={`w-full max-w-4xl min-h-[100px] p-2 text-gray-500 bg-white border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent`}
           maxLength={120}
-          value={payload.description}
+          value={payload.bio}
           onChange={(e) => {
             setPayload((prev) => ({
               ...prev,
-              description: e.target.value,
+              bio: e.target.value,
             }));
           }}
         />
         <p className={`text-xs text-gray-500`}>
-          {payload.description.length} / {120}
+          {payload.bio.length} / {120}
         </p>
       </div>
       <div className={`flex flex-col items-start w-full gap-4`}>
@@ -141,7 +183,7 @@ const SignupTemplate = () => {
             type={`text`}
             className={`w-full p-1.5 text-sm text-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent`}
             placeholder={`instagram id`}
-            value={payload.socialLinks.website}
+            value={payload.socialLinks.instagram}
             onChange={(e) => {
               setPayload((prev) => ({
                 ...prev,
@@ -163,7 +205,7 @@ const SignupTemplate = () => {
             type={`text`}
             className={`w-full p-1.5 text-sm text-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent`}
             placeholder={`twitter id`}
-            value={payload.socialLinks.website}
+            value={payload.socialLinks.twitter}
             onChange={(e) => {
               setPayload((prev) => ({
                 ...prev,
@@ -179,6 +221,13 @@ const SignupTemplate = () => {
       <button
         disabled={isSubmitDisabled}
         className={`w-full max-w-[120px] p-2 text-sm text-white font-bold bg-primary rounded-md disabled:opacity-50 disabled:cursor-not-allowed`}
+        onClick={() => {
+          if (!isOAuthSignIn || !payload.email) {
+            alert(`Please sign in with Google`);
+            return;
+          }
+          handleSignup(payload);
+        }}
       >
         Sign up
       </button>
