5495d1570f1a4fe4263d25666e35b98544250b4a
tedlee <ted.lee@bclabs.co.kr>
2023-09-14 22:00:36 +0900

    fix: added react-query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Footer from "@/components/common/Footer";
 import NextAuthSessionProvider from "@/components/common/module/NextAuthSessionProvider";
 import Header from "@/components/common/header/Header";
+import ReactQueryClientProvider from "@/components/common/module/ReactQueryClientProvider";
 
 export const metadata = {
   title: "soullink",
@@ -17,13 +18,15 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NextAuthSessionProvider>
-          <Header />
-          <main
-            className={`xs:my-10 py-12 xs:py-2 xs:px-4 xl:px-24 3xl:px-48 desktop:px-[400px] bg-white `}
-          >
-            {children}
-          </main>
-          <Footer />
+          <ReactQueryClientProvider>
+            <Header />
+            <main
+              className={`xs:my-10 py-12 xs:py-2 xs:px-4 xl:px-24 3xl:px-48 desktop:px-[400px] bg-white `}
+            >
+              {children}
+            </main>
+            <Footer />
+          </ReactQueryClientProvider>
         </NextAuthSessionProvider>
       </body>
     </html>
