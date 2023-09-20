323b11b467eec39783754cdf9d21da46f0ae17fb
tedlee <ted.lee@bclabs.co.kr>
2023-09-20 23:33:54 +0900

    fix: applied data api & static paths

--- app/playlist/[id]/page.tsx
+++ app/playlist/[id]/page.tsx
@@ -1,14 +1,52 @@
 import React from "react";
 import DetailTemplate from "@/components/detail/DetailTemplate";
+import {
+  getPlaylistsPaths,
+  getSinglePlaylist,
+} from "@/libs/utils/client/fetchers";
+import process from "process";
 
-import { fakeFirstPlaylistData } from "@/libs/utils/client/commonStaticApiData";
+const Page = async ({ params: { id } }: { params: { id: string } }) => {
+  const playlistData = await getSinglePlaylist(id);
 
-const Page = ({ params: { id } }: { params: { id: string } }) => {
-  return <DetailTemplate playlistData={fakeFirstPlaylistData} />;
+  return <DetailTemplate playlistData={playlistData} />;
 };
 
 export default Page;
 
 export const generateStaticParams = async () => {
-  return [1, 2, 3, 4];
+  const paths = await getPlaylistsPaths();
+
+  return paths;
 };
+
+export async function generateMetadata({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  if (!id) {
+    return {};
+  }
+
+  const data = await getSinglePlaylist(id);
+
+  if (!data) {
+    return {};
+  }
+
+  const ImageSrc =
+    data?.coverImage ??
+    `${process.env.NEXT_APP_BASE_URL}/image/common/default_cover_image.svg`;
+  const SEOImage = ImageSrc?.substring(1, ImageSrc.length);
+
+  return {
+    title: `${data?.title || ""} on soullink`,
+    description: data?.description || "",
+    openGraph: {
+      title: `${data?.title || ""} on soullink`,
+      description: data?.description || "",
+      images: ImageSrc,
+    },
+  };
+}
