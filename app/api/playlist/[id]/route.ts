18ce81976142b3d981ac4c1c171c5b835c9f6241
tedlee <ted.lee@bclabs.co.kr>
2023-09-20 23:28:13 +0900

    feat: playlist detail page api

--- app/api/playlist/[id]/route.ts
+++ app/api/playlist/[id]/route.ts
@@ -1,17 +1,13 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/prisma/client";
 
-// Playlist Detail page get 요청 예시
-
 export async function GET(req: Request) {
   try {
     const pathname = new URL(req.url).pathname.split("/");
-    // todo playlist id -> playlist title로 페이지 route 변경 필요할 수도 있음.
-    // todo play 재생시 client에서 재생 횟수 추가 요청 api 필요
-    const id = pathname[pathname.length - 1].replace("-", " ");
+    const title = pathname[pathname.length - 1].replace("-", " ");
     const playlist = await prisma.playlist.findUnique({
       where: {
-        id,
+        title,
       },
       select: {
         id: true,
@@ -19,7 +15,16 @@ export async function GET(req: Request) {
         updatedAt: true,
         title: true,
         description: true,
-        songs: true,
+        songs: {
+          select: {
+            id: true,
+            title: true,
+            artist: true,
+            thumbnail: true,
+            playedCount: true,
+            likedUsers: true,
+          },
+        },
         likedBy: true,
         playCount: true,
       },
@@ -27,7 +32,7 @@ export async function GET(req: Request) {
 
     return NextResponse.json(
       {
-        playlistData: playlist,
+        data: playlist,
       },
       {
         status: 200,
