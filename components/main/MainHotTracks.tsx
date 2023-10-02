7ea936eebdbce08b9070b6ad69513d09de4a27da
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 19:00:15 +0900

    fix: added user session info to table component props

--- components/main/MainHotTracks.tsx
+++ components/main/MainHotTracks.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import React from "react";
 import Title from "@/components/common/module/Title";
 import Table from "@/components/common/module/Table";
 import { SongType } from "@/libs/types/common/Song&PlaylistType";
+import { UserSessionType } from "@/libs/types/common/userType";
+import { useSession } from "next-auth/react";
 
 const MainHotTracks = ({ songList }: { songList: SongType[] }) => {
+  const { data: session } = useSession() as { data: UserSessionType };
   return (
     <section className={`flex flex-col items-start w-full gap-4`}>
       <Title size={`h1`} text={`Popular Tracks`} />
-      <Table songList={songList} />
+      <Table songList={songList} userId={session?.userId} />
     </section>
   );
 };
