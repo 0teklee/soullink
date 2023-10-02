3e4de3f3ec856d04f99ac80bc83602277bac083b
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:42:43 +0900

    wip: main playlist get apis

---
+++ app/api/playlist/list/trending/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
+
+export async function GET(req: Request) {
+  try {
+    const trendingPlaylist = await prisma.playlist.findMany({
+      take: 20,
+      orderBy: {
+        likedCount: "desc",
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        coverImage: true,
+        createdAt: true,
+        likedCount: true,
+        author: {
+          select: {
+            id: true,
+            nickname: true,
+          },
+        },
+        authorId: true,
+        playCount: true,
+        songs: true,
+      },
+    });
+    return new NextResponse(
+      JSON.stringify({
+        message: "success",
+        trendingPlayLists: trendingPlaylist,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      },
+    );
+  } catch (err) {
+    console.log("write.ts error: ", err);
+    return new NextResponse(JSON.stringify({ message: "fail" }), {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+}
