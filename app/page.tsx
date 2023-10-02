f732a05ebc30461e99ce7d3244226c8970be660e
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:37:18 +0900

    fix: applied api data on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 import { Inter } from "next/font/google";
 import MainTemplate from "@/components/main/MainTemplate";
+import {
+  getTrendingPlaylists,
+  getTrendingSongs,
+} from "@/libs/utils/client/fetchers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 // api 함수
 
-export default function Home() {
+const Home = async () => {
+  const getMainData = await getTrendingPlaylists();
+  const getHotTracks = await getTrendingSongs();
+  const tempProps = {
+    trendingPlayLists: getMainData,
+    friendsPlayLists: getMainData,
+    hotTracks: getHotTracks,
+    myHistory: getMainData,
+  };
   return (
     <section className={`py-6`}>
-      <MainTemplate />
+      <MainTemplate propsData={tempProps} />
     </section>
   );
-}
+};
+
+export default Home;
