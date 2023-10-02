302c4868c534da08bc07988bcbcb66447854c092
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:24:45 +0900

    fix: added new comment component props, user session props

--- components/user/UserTemplate.tsx
+++ components/user/UserTemplate.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import React from "react";
-import { UserType } from "@/types/common/userType";
-import UserHeader from "@/components/user/UserHeader";
+import { UserSessionType, UserType } from "@/libs/types/common/userType";
+import UserHeader from "@/components/user/module/UserHeader";
 import Title from "@/components/common/module/Title";
 import PlayListSlider from "@/components/common/playlist/PlayListSlider";
 import CommentContainer from "@/components/common/comments/CommentContainer";
+import { useSession } from "next-auth/react";
 
 const UserTemplate = ({ userProps }: { userProps: UserType }) => {
-  const { nickname, playlists, myComments } = userProps;
-  const isPlaylistsEmpty = playlists.length === 0;
+  const { data: session } = useSession() as { data: UserSessionType };
+  const { nickname, createdPlaylists, id: profileId } = userProps;
+  const isPlaylistsEmpty = createdPlaylists?.length === 0;
+
   return (
     <section className={`flex flex-col items-center gap-10 pb-10`}>
       <UserHeader userProfile={userProps} />
@@ -17,12 +20,16 @@ const UserTemplate = ({ userProps }: { userProps: UserType }) => {
         className={`flex flex-col gap-3 items-start w-full text-gray-900 mt-[520px]`}
       >
         <Title size={`h1`} text={`${nickname}'s Playlist`} />
-        {!isPlaylistsEmpty && <PlayListSlider playLists={playlists} />}
+        {!isPlaylistsEmpty && <PlayListSlider playLists={createdPlaylists} />}
         {isPlaylistsEmpty && <Title size={`h2`} text={`No Playlists`} />}
       </div>
       <div className={`flex flex-col gap-3 items-start w-full text-gray-900`}>
-        <Title size={`h1`} text={`Guestbook`} />
-        <CommentContainer commentList={myComments} />
+        <Title size={`h2`} text={`Guestbook`} />
+        <CommentContainer
+          postId={profileId}
+          userId={session?.userId}
+          isProfile={true}
+        />
       </div>
     </section>
   );
