b736da1a31ea89dd790b8f8659b7f81a8fcb66c9
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:18:07 +0900

    feat: added comment input

--- components/common/comments/CommentContainer.tsx
+++ components/common/comments/CommentContainer.tsx
@@ -1,19 +1,56 @@
-import React from "react";
-import { CommentType } from "@/types/common/userType";
+"use client";
+import React, { useEffect } from "react";
+import { CommentType, UserSessionType } from "@/libs/types/common/userType";
 import CommentItem from "@/components/common/comments/CommentItem";
+import CommentInput from "@/components/common/comments/CommentInput";
+import { getComments } from "@/libs/utils/client/fetchers";
+import { useQuery } from "react-query";
 
-const CommentContainer = ({ commentList }: { commentList: CommentType[] }) => {
+const CommentContainer = ({
+  postId,
+  userId,
+  isProfile,
+}: {
+  postId: string;
+  userId?: string;
+  isProfile?: boolean;
+}) => {
+  const {
+    data: commentsData,
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["commentList", postId, userId],
+    queryFn: () => getComments(postId, userId, isProfile),
+    enabled: !!postId && !!userId,
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [userId]);
+
   return (
-    <div className={`flex flex-col justify-start items-start w-full gap-y-3`}>
-      {commentList ? (
-        commentList.map((comment: CommentType, index) => (
+    <div
+      className={`flex flex-col justify-start items-start w-full gap-y-3 px-4`}
+    >
+      <CommentInput postId={postId} isProfile={isProfile} refetch={refetch} />
+      {!isLoading &&
+        commentsData &&
+        commentsData.length > 0 &&
+        commentsData.map((comment: CommentType, index) => (
           <CommentItem
+            postId={postId}
+            userId={userId}
             commentProps={comment}
             key={`comment_${comment.id}_${index}`}
+            refetch={refetch}
           />
-        ))
-      ) : (
+        ))}{" "}
+      {!isLoading && commentsData && commentsData.length === 0 && (
         <p className={`text-gray-300 text-lg font-normal`}>No comments yet.</p>
+      )}
+      {isLoading && (
+        <p className={`text-gray-300 text-lg font-normal`}>Loading...</p>
       )}
     </div>
   );
