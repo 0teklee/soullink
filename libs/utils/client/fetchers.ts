b98f959731a507fd56def13c5f30d62ba87cb8d2
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:41:33 +0900

    add: user, comment getm post fetchers + get trending songs fetcher

--- libs/utils/client/fetchers.ts
+++ libs/utils/client/fetchers.ts
@@ -2,7 +2,19 @@ import {
   PlaylistLikeType,
   PlaylistType,
   SongLikeType,
-} from "@/types/common/Song&PlaylistType";
+  SongType,
+} from "@/libs/types/common/Song&PlaylistType";
+import {
+  CommentPayloadType,
+  CommentType,
+  PayloadCommentDeleteType,
+  PayloadCommentLikeType,
+  PostFollowType,
+  UserType,
+} from "@/libs/types/common/userType";
+
+/* Playlist Fetchers */
+/* GET */
 
 export const getSinglePlaylist = async (id: string): Promise<PlaylistType> => {
   const res = await fetch(
@@ -23,6 +35,16 @@ export const getPlaylistsPaths = async (): Promise<string[]> => {
   return paths;
 };
 
+export const getTrendingPlaylists = async (): Promise<PlaylistType[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_APP_BASE_URL}/api/playlist/list/trending`,
+  );
+  const data = await res.json();
+  return data.trendingPlayLists;
+};
+
+/* POST */
+
 export const postPlaylistLike = async (request: PlaylistLikeType) => {
   const { playlistId } = request;
   const res = await fetch(
@@ -39,6 +61,19 @@ export const postPlaylistLike = async (request: PlaylistLikeType) => {
   return data;
 };
 
+/* Song Fetchers */
+/* GET */
+
+export const getTrendingSongs = async (): Promise<SongType[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_APP_BASE_URL}/api/song/list/trending`,
+  );
+  const data = await res.json();
+  return data.trendingData;
+};
+
+/* POST */
+
 export const postSongLike = async (request: SongLikeType) => {
   const { songId } = request;
   const res = await fetch(
@@ -51,6 +86,93 @@ export const postSongLike = async (request: SongLikeType) => {
       },
     },
   );
+  const data = await res.json();
+  return data;
+};
+
+/* User Fetchers */
+/* GET */
+
+export const getUsersPaths = async (): Promise<string[]> => {
+  const res = await fetch(`${process.env.NEXT_APP_BASE_URL}/api/user/paths`);
+  const data = await res.json();
+  const paths = data.userNicknamePaths;
+  return paths;
+};
+
+export const getSingleUserProfile = async (id: string): Promise<UserType> => {
+  const res = await fetch(`${process.env.NEXT_APP_BASE_URL}/api/user/${id}`);
+  const resData: Promise<UserType> = await res.json().then((data) => data.user);
+  return resData;
+};
+
+export const getComments = async (
+  id: string,
+  visitorId?: string,
+  isProfile?: boolean,
+): Promise<CommentType[]> => {
+  const res = await fetch(
+    `${process.env.NEXT_APP_BASE_URL}/api/comment/${id}?userId=${visitorId}&isProfile=${isProfile}`,
+  );
+
+  const resData: Promise<CommentType[]> = await res
+    .json()
+    .then((data) => data.comments);
+  return resData;
+};
+
+/* POST */
+
+export const postComment = async (request: CommentPayloadType) => {
+  const res = await fetch(
+    `${process.env.NEXT_APP_BASE_URL}/api/comment/create`,
+    {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const data = await res.json();
+  return data;
+};
+
+export const postLikeComment = async (request: PayloadCommentLikeType) => {
+  const res = await fetch(`${process.env.NEXT_APP_BASE_URL}/api/comment/like`, {
+    method: "POST",
+    body: JSON.stringify(request),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const postDeleteComment = async (request: PayloadCommentDeleteType) => {
+  const res = await fetch(
+    `${process.env.NEXT_APP_BASE_URL}/api/comment/delete`,
+    {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  const data = await res.json();
+  return data;
+};
+
+export const postUserFollow = async (request: PostFollowType) => {
+  const res = await fetch(`${process.env.NEXT_APP_BASE_URL}/api/user/follow`, {
+    method: "POST",
+    body: JSON.stringify(request),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const data = await res.json();
   return data;
 };
