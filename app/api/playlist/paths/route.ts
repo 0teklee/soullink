29e73c8a6cad90d50aeb509b2a5adf5010d19a5b
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:31:58 +0900

    fix: fixed pathname to encodedURI

--- app/api/playlist/paths/route.ts
+++ app/api/playlist/paths/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: Request) {
           title: true,
         },
       })
-      .then((res) => res.map((item) => item.title.replace(" ", "-")));
+      .then((res) => res.map((item) => encodeURIComponent(item.title)));
 
     return NextResponse.json(
       {
@@ -22,7 +22,7 @@ export async function GET(req: Request) {
       },
     );
   } catch (err) {
-    console.log("post detail error: ", err);
+    console.log("post playlistDetail error: ", err);
     return new NextResponse(
       JSON.stringify({ message: "fail", errorCode: 404 }),
       {
