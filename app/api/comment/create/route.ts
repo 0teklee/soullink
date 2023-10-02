b31fd0f0834a0e552f7bc2f09e6630c9f890e8cd
tedlee <ted.lee@bclabs.co.kr>
2023-10-02 14:33:16 +0900

    feat: comment create api added

---
+++ app/api/comment/create/route.ts
@@ -0,0 +1,76 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
+import { CommentPayloadType } from "@/libs/types/common/userType";
+
+export async function POST(req: Request) {
+  try {
+    const reqJson = await req.json();
+    const {
+      userId,
+      postId,
+      comment,
+      isProfile,
+      isPrivate,
+    }: CommentPayloadType = reqJson;
+
+    if (isProfile) {
+      const postProfileComment = await prisma.comment.create({
+        data: {
+          author: {
+            connect: {
+              id: userId,
+            },
+          },
+          profile: {
+            connect: {
+              id: postId,
+            },
+          },
+          comment,
+          isPrivate,
+          type: "USER",
+        },
+      });
+
+      return new NextResponse(
+        JSON.stringify({ message: "success", postProfileComment }),
+        {
+          status: 200,
+        },
+      );
+    }
+    const postComment = await prisma.comment.create({
+      data: {
+        author: {
+          connect: {
+            id: userId,
+          },
+        },
+        playlist: {
+          connect: {
+            id: postId,
+          },
+        },
+        type: "PLAYLIST",
+        isPrivate,
+        comment,
+      },
+    });
+
+    return new NextResponse(
+      JSON.stringify({ message: "success", postComment }),
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.log("post playlistDetail error: ", err);
+    return new NextResponse(
+      JSON.stringify({ message: "fail", errorCode: 404 }),
+      {
+        status: 500,
+        statusText: "Internal Server Error",
+      },
+    );
+  }
+}
