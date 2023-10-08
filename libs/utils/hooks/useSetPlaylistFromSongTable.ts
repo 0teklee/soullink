36774796a848bcb375b73c5b7a254561e8579b89
tedlee <ted.lee@bclabs.co.kr>
2023-10-08 14:41:46 +0900

    feat: added play song from table hook

---
+++ libs/utils/hooks/useSetPlaylistFromSongTable.ts
@@ -0,0 +1,58 @@
+import { PlaylistType } from "@/libs/types/common/Song&PlaylistType";
+import { useRecoilState } from "recoil";
+import { playlistState } from "@/libs/recoil/playlistAtom";
+import { playerGlobalState } from "@/libs/recoil/playerAtom";
+
+export const useSetPlaylistFromSongTable = (
+  songId: string,
+  targetPlaylist?: PlaylistType,
+) => {
+  const [currentPlaylist, setCurrentPlaylist] = useRecoilState(playlistState);
+  const [currentPlayer, setCurrentPlayer] = useRecoilState(playerGlobalState);
+
+  if (!targetPlaylist) {
+    return;
+  }
+
+  const { songs: targetSongs } = targetPlaylist;
+  const targetSongIndex = targetSongs.findIndex((song) => song.id === songId);
+
+  const isSongPlaying =
+    currentPlaylist &&
+    currentPlayer &&
+    currentPlayer.playing &&
+    targetPlaylist.id === currentPlaylist?.id &&
+    songId === currentPlaylist?.songs[currentPlayer.currentSongListIndex]?.id;
+
+  const updatePlaylist = (playlist: PlaylistType) => {
+    setCurrentPlaylist(playlist);
+    setCurrentPlayer({
+      ...currentPlayer,
+      currentSongListIndex: targetSongIndex || 0,
+      playing: true,
+    });
+  };
+
+  const updateCurrentPlaySong = (playlist: PlaylistType) => {
+    if (currentPlayer.playing && isSongPlaying) {
+      setCurrentPlayer((prev) => ({ ...prev, playing: false }));
+      return;
+    }
+
+    setCurrentPlayer({
+      ...currentPlayer,
+      currentSongListIndex: targetSongIndex,
+      playing: true,
+    });
+  };
+
+  const playSongFromTable = (playlist: PlaylistType) => {
+    if (!currentPlaylist || currentPlaylist.id !== playlist.id) {
+      return updatePlaylist(playlist);
+    }
+
+    return updateCurrentPlaySong(playlist);
+  };
+
+  return { playSongFromTable, currentPlayer, currentPlaylist, isSongPlaying };
+};
