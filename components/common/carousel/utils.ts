7212bb4efec9f8ce53eed7b93f092825545b70d8
tedlee <ted.lee@bclabs.co.kr>
2023-11-04 02:12:03 +0900

    fix: card slider motion utils fixed

--- components/common/carousel/utils.ts
+++ components/common/carousel/utils.ts
@@ -1,93 +1,46 @@
-const getDirection = <T>(index: number, activeIndex: number, arr: T[]) => {
-  console.log(index, activeIndex, arr.length / 2, arr.length / 2 > index);
-  return arr.length / 2 > index ? 1 : -1;
+enum ZIndex {
+  CenterSides = 1,
+  SideEnds = 0,
+}
+
+enum Opacity {
+  CenterSides = 0.7,
+  SideEnds = 0.5,
+}
+
+enum Scale {
+  CenterSides = 0.9,
+  SideEnds = 0.5,
+}
+
+enum Direction {
+  Left = -1,
+  Right = 1,
+}
+
+const { Left, Right } = Direction;
+
+const getZIndex = (distanceToCenter: number) => {
+  return distanceToCenter === 2 ? ZIndex.SideEnds : ZIndex.CenterSides;
 };
 
-const getIsCenterSide = <T>(index: number, activeIndex: number, arr: T[]) => {
-  return (
-    activeIndex === index + 1 ||
-    activeIndex === index - 1 ||
-    (activeIndex === arr.length - 1 && index === 0) ||
-    (activeIndex === 0 && index === arr.length - 1)
-  );
+const getOpacity = (distanceToCenter: number) => {
+  return distanceToCenter === 2 ? Opacity.SideEnds : Opacity.CenterSides;
 };
 
-const getIsSideEnds = <T>(index: number, activeIndex: number, arr: T[]) => {
-  return (
-    activeIndex === index + 2 ||
-    activeIndex === index - 2 ||
-    (activeIndex === arr.length - 1 && index === 1) ||
-    (activeIndex === 0 && index === arr.length - 2)
-  );
+const getScale = (distanceToCenter: number) => {
+  return distanceToCenter === 2 ? Scale.SideEnds : Scale.CenterSides;
 };
 
-const getPositions = <T>(index: number, activeIndex: number, arr: T[]) => {
-  return {
-    direction: getDirection(index, activeIndex, arr),
-    isCenterSide: getIsCenterSide(index, activeIndex, arr),
-    isSideEnds: getIsSideEnds(index, activeIndex, arr),
-  };
+const getDirection = (activeIndex: number, index: number) => {
+  const distance = (index - activeIndex + 5) % 5;
+  return distance > 2 ? Left : Right;
 };
 
-const setZIndex = <T>(index: number, activeIndex: number, arr: T[]) => {
-  const { isCenterSide, isSideEnds } = getPositions(index, activeIndex, arr);
-
-  if (isCenterSide) {
-    return 1;
-  }
-  if (isSideEnds) {
-    return 0;
-  }
-
-  return -1;
-};
-
-const setOpacity = <T>(index: number, activeIndex: number, arr: T[]) => {
-  const { isCenterSide, isSideEnds } = getPositions(index, activeIndex, arr);
-
-  if (isCenterSide) {
-    return 0.7;
-  }
-
-  if (isSideEnds) {
-    return 0.4;
-  }
-
-  if (index === 0 && activeIndex === arr.length - 1) {
-    return 0.4;
-  }
-
-  return 0;
-};
-
-const setXindex = <T>(index: number, activeIndex: number, arr: T[]) => {
-  const { isCenterSide, isSideEnds, direction } = getPositions(
-    index,
-    activeIndex,
-    arr,
-  );
-
-  if (isCenterSide) {
-    return 120 * direction;
-  }
-
-  if (isSideEnds) {
-    return 240 * direction;
-  }
-
-  return 0;
-};
-
-const setScale = <T>(index: number, activeIndex: number, arr: T[]) => {
-  const { isCenterSide, isSideEnds } = getPositions(index, activeIndex, arr);
-
-  if (isCenterSide) {
-    return 0.9;
-  }
-
-  if (isSideEnds) {
-    return 0.8;
-  }
+const getXIndex = (distanceToCenter: number, direction: number) => {
+  return distanceToCenter === 2
+    ? 240 * direction
+    : 120 * (2 - distanceToCenter) * direction;
 };
 
 const getItemMotionInitial = <T>(
@@ -95,10 +48,17 @@ const getItemMotionInitial = <T>(
   arr: T[],
   activeIndex: number,
 ) => {
-  const zIndex = setZIndex(index, activeIndex, arr);
-  const xIndex = setXindex(index, activeIndex, arr);
-  const opacity = setOpacity(index, activeIndex, arr);
-  const scale = setScale(index, activeIndex, arr);
+  const relativeIndex = (index - activeIndex + arr.length) % arr.length;
+  const distanceToCenter = Math.min(
+    relativeIndex,
+    Math.abs(arr.length - relativeIndex),
+  );
+
+  const zIndex = getZIndex(distanceToCenter);
+  const opacity = getOpacity(distanceToCenter);
+  const scale = getScale(distanceToCenter);
+  const direction = getDirection(activeIndex, index);
+  const xIndex = getXIndex(distanceToCenter, direction);
 
   const transition = {
     duration: 0.5,
@@ -125,11 +85,4 @@ const getItemMotionInitial = <T>(
   };
 };
 
-const sortArr = <T>(arr: T[], activeIndex: number) => {
-  const beforeArr = arr.slice(0, activeIndex);
-  const afterArr = arr.slice(activeIndex, arr.length);
-
-  return afterArr.concat(beforeArr);
-};
-
-export { getItemMotionInitial, setZIndex, sortArr };
+export { getItemMotionInitial };
