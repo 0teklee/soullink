dc0953ff393012e57ee8de8895a8abf704f8ce23
tedlee <ted.lee@bclabs.co.kr>
2023-11-04 02:12:23 +0900

    fix: mood font color formatting utils added

--- components/playlistCreate/utils.ts
+++ components/playlistCreate/utils.ts
@@ -1,7 +1,10 @@
-export const formatMoodColor = (mood: string, isHover?: boolean) => {
+import { PlaylistMoodType } from "@/libs/types/common/Song&PlaylistType";
+
+export const formatMoodBgColor = (
+  mood: PlaylistMoodType,
+  isHover?: boolean,
+) => {
   switch (mood) {
-    case "energetic":
-      return isHover ? "hover:bg-[#ff3b29]" : "bg-[#ff3b29]";
     case "upbeat":
       return isHover ? "hover:bg-[#FF6F00]" : "bg-[#FF6F00]";
     case "chill":
@@ -10,9 +13,25 @@ export const formatMoodColor = (mood: string, isHover?: boolean) => {
       return isHover ? "hover:bg-[#2e70ff]" : "bg-[#2e70ff]";
     case "melancholic":
       return isHover ? "hover:bg-[#6e00d0]" : "bg-[#6e00d0]";
-    case "dark":
-      return isHover ? "hover:bg-[#0e0a18]" : "bg-[#0e0a18]";
     default:
       return isHover ? "hover:bg-[#01FF00]" : "bg-[#01FF00]";
+  }
+};
+
+export const formatMoodFontColor = (
+  mood: PlaylistMoodType,
+  isHover?: boolean,
+) => {
+  switch (mood) {
+    case "upbeat":
+      return isHover ? "hover:text-[#FF6F00]" : "text-[#FF6F00]";
+    case "chill":
+      return isHover ? "hover:text-[#9cf440]" : "text-[#9cf440]";
+    case "relaxed":
+      return isHover ? "hover:text-[#2e70ff]" : "text-[#2e70ff]";
+    case "melancholic":
+      return isHover ? "hover:text-[#6e00d0]" : "text-[#6e00d0]";
+    default:
+      return isHover ? "hover:text-[#01FF00]" : "text-[#01FF00]";
   }
 };
