aa1dda03c245b964be9947a369fc5bfd2b20bdba
tedlee <ted.lee@bclabs.co.kr>
2023-11-04 02:11:28 +0900

    fix: card slider container logic fixed

--- components/common/carousel/img-card/ImageCardContainer.tsx
+++ components/common/carousel/img-card/ImageCardContainer.tsx
@@ -1,101 +1,119 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { PlaylistType } from "@/libs/types/common/Song&PlaylistType";
 import { AnimatePresence, motion, useAnimationControls } from "framer-motion";
 import ImageCardItem from "@/components/common/carousel/img-card/ImageCardItem";
-import Title from "@/components/common/module/Title";
 import { fillEmptyPlaylist } from "@/libs/utils/client/commonUtils";
-import {
-  getItemMotionInitial,
-  sortArr,
-} from "@/components/common/carousel/utils";
+import { getItemMotionInitial } from "@/components/common/carousel/utils";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import { interval5Seconds } from "@/libs/utils/client/commonValues";
 
-const ImageCardContainer = ({ playlists }: { playlists: PlaylistType[] }) => {
+const ImageCardContainer = ({ playlists }: { playlists?: PlaylistType[] }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const processedLists = sortArr(fillEmptyPlaylist(playlists), activeIndex);
+  const processedLists = fillEmptyPlaylist(playlists || []);
 
   const controls = useAnimationControls();
 
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setActiveIndex((prevIndex) => {
-  //       if (prevIndex === processedLists.length - 1) return 0;
-  //       return prevIndex + 1;
-  //     });
-  //   }, 2500);
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, []);
+  const handlePrev = () => {
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === 0 && !processedLists[processedLists.length - 1].title) {
+        return prevIndex;
+      }
+
+      if (!processedLists[prevIndex - 1].title) {
+        return prevIndex;
+      }
+
+      if (prevIndex === 0) {
+        return processedLists.length - 1;
+      }
+      return prevIndex - 1;
+    });
+  };
+
+  const handleNext = () => {
+    setActiveIndex((prevIndex) => {
+      if (!processedLists[prevIndex + 1].title) {
+        return prevIndex;
+      }
+
+      if (prevIndex === processedLists.length - 1) {
+        return 0;
+      }
+      return prevIndex + 1;
+    });
+  };
 
   useEffect(() => {
-    // update motion.div control
+    const intervalId = setInterval(() => {
+      setActiveIndex((prevIndex) => {
+        if (!processedLists[prevIndex + 1].title) {
+          return prevIndex;
+        }
+
+        if (prevIndex === processedLists.length - 1) return 0;
+        return prevIndex + 1;
+      });
+    }, interval5Seconds);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
     controls.start("active");
-    console.log("target processLists: ", sortArr(processedLists, activeIndex));
   }, [activeIndex]);
 
   return (
     <div
-      className={`relative flex items-center justify-center w-full h-96 py-2`}
+      className={`relative flex items-center justify-center w-full h-60 py-2 group`}
     >
       <AnimatePresence>
-        {!!playlists &&
-          playlists.length > 0 &&
-          processedLists.map((playlist, itemIndex, arr) => {
-            const { initial, center } = getItemMotionInitial(
-              itemIndex,
-              arr,
-              activeIndex,
-            );
-
-            return (
+        {processedLists.map((playlist, itemIndex, arr) => {
+          const { initial, center } = getItemMotionInitial(
+            itemIndex,
+            arr,
+            activeIndex,
+          );
+          return (
+            <Fragment
+              key={`${playlist.mood.name}_${itemIndex}_${playlist.title}_container`}
+            >
               <motion.div
-                className={`absolute playlist_card_${itemIndex}`}
-                key={`${playlist.mood}_${itemIndex}`}
+                className={`absolute playlist_card_${itemIndex} h-full`}
                 initial={initial}
                 animate={activeIndex === itemIndex ? center : initial}
+                drag={`x`}
+                dragConstraints={{ left: 0, right: 50 }}
+                onDragEnd={(e, info) => {
+                  if (info.offset.x > 0) {
+                    handlePrev();
+                  } else {
+                    handleNext();
+                  }
+                }}
               >
                 <ImageCardItem
-                  key={itemIndex}
                   playlist={playlist}
                   index={itemIndex}
+                  activeIndex={activeIndex}
                   setActiveIndex={setActiveIndex}
                 />
               </motion.div>
-            );
-          })}
+            </Fragment>
+          );
+        })}
         <div
-          className={`absolute bottom-2 flex gap-3 items-center justify-center text-gray-900 font-semibold`}
+          className={`absolute top-1/2 -translate-y-1/2 flex gap-3 items-center justify-between w-full group-hover:text-primary group-hover:opacity-100 opacity-0 xs:z-30`}
         >
-          <button
-            onClick={() => {
-              setActiveIndex((prevIndex) => {
-                if (prevIndex === 0) return processedLists.length - 1;
-                return prevIndex - 1;
-              });
-
-              console.log("prev activeIndex", activeIndex);
-            }}
-          >
-            prev
+          <button onClick={handlePrev}>
+            <ChevronLeftIcon className={`w-12 h-12 mix-blend-difference`} />
           </button>
-          <button
-            onClick={() => {
-              setActiveIndex((prevIndex) => {
-                if (prevIndex === processedLists.length - 1) return 0;
-                return prevIndex + 1;
-              });
-              console.log("next activeIndex", activeIndex);
-            }}
-          >
-            next
+          <button onClick={handleNext}>
+            <ChevronRightIcon className={`w-12 h-12 mix-blend-difference`} />
           </button>
         </div>
-        {/*{!!playlists ||*/}
-        {/*  (playlists.length === 0 && (*/}
-        {/*    <Title size={`h3`} text={`No playlists yet`} />*/}
-        {/*  ))}*/}
       </AnimatePresence>
     </div>
   );
