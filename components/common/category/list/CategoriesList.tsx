af30dd1acbbe9242e4afd50b74a5dd696f1dd9ce
tedlee <ted.lee@bclabs.co.kr>
2023-11-14 10:29:35 +0900

    add: Common categories list filter added

---
+++ components/common/category/list/CategoriesList.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const CategoriesList = ({
+  categories,
+}: {
+  categories: string[] | { name: string }[];
+}) => {
+  const router = useRouter();
+
+  const categoriesArr = categories.map((category_item) =>
+    typeof category_item === "string" ? category_item : category_item.name,
+  );
+
+  return (
+    <div
+      className={`flex items-center gap-3 text-sm text-gray-700  lg:text-xs `}
+    >
+      {categoriesArr.map((category_item, index) => (
+        <button
+          onClick={() => router.push(`/category?category=${category_item}`)}
+          className={`px-2 py-1 border border-gray-300 rounded hover:bg-primary hover:text-white hover:border-primary cursor-pointer`}
+          key={`${category_item}_${category_item}_${index}`}
+        >
+          {category_item}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesList;
