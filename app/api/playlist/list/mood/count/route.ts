344e679e700bad150913aa20d39b9017888d7422
tedlee <ted.lee@bclabs.co.kr>
2023-11-14 10:41:25 +0900

    add: mood list api filters (date, keyword) added (no playlist)

---
+++ app/api/playlist/list/mood/count/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
+import { formatDateFilter } from "@/libs/utils/server/formatter";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const { recent } = Object.fromEntries(url.searchParams.entries()) as {
+    recent: string;
+  };
+
+  const recentDate = formatDateFilter(recent);
+
+  try {
+    const moodPlayLists = await prisma.playlist.findMany({
+      where: {
+        OR: [
+          {
+            recentPlay: {
+              every: {
+                createdAt: {
+                  gte: recentDate,
+                },
+              },
+            },
+          },
+          {
+            createdAt: {
+              gte: recentDate,
+            },
+          },
+        ],
+      },
+      take: 10,
+      orderBy: {
+        likedCount: "desc",
+      },
+      select: {
+        _count: true,
+        mood: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    const moodCount = moodPlayLists
+      .map((item: { mood: { name: string } }) => {
+        return item.mood.name;
+      })
+      .reduce((acc: Record<string, number>, cur: string) => {
+        acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+      }, {});
+
+    const moodCountArray: { name: string; count: number }[] = Object.entries(
+      moodCount,
+    ).map(([moodName, countNumber]) => ({
+      name: moodName,
+      count: countNumber,
+    }));
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "success",
+        moodCount: moodCountArray,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      },
+    );
+  } catch (err) {
+    console.log("write.ts error: ", err);
+    return new NextResponse(JSON.stringify({ message: "fail" }), {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+}
