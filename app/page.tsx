1d182908d8dffa8bb3fca601a2b58fcc844e4117
tedlee <ted.lee@bclabs.co.kr>
2023-11-14 10:51:58 +0900

    fix: fetcher name fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 import { Inter } from "next/font/google";
 import MainTemplate from "@/components/main/MainTemplate";
 import {
-  getTrendingPlaylists,
+  getMainPageTrendingPlaylists,
   getTrendingSongs,
 } from "@/libs/utils/client/fetchers";
 import PlaylistUpdateProvider from "@/components/common/playlist/PlaylistUpdateProvider";
 import { PlaylistType } from "@/libs/types/common/Song&PlaylistType";
-import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = async () => {
-  const getMainData = await getTrendingPlaylists();
+  const getMainData = await getMainPageTrendingPlaylists();
   const getHotTracks = await getTrendingSongs();
   const tempProps = {
     trendingPlayLists: getMainData,
