4cf08de12313f93ee4bfe45ba025451320b42c0f
tedlee <ted.lee@bclabs.co.kr>
2023-11-14 10:36:13 +0900

    feat: search user api

---
+++ app/api/search/user/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
+import {
+  formatDateFilter,
+  formatSearchOrderBy,
+} from "@/libs/utils/server/formatter";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const { keyword, orderBy } = Object.fromEntries(url.searchParams.entries());
+
+  try {
+    const users = await prisma.user.findMany({
+      take: 20,
+      orderBy: [
+        {
+          _relevance: {
+            fields: "nickname",
+            search: keyword,
+            sort: "desc",
+          },
+        },
+        {
+          nickname: "asc",
+        },
+      ],
+      select: {
+        id: true,
+        nickname: true,
+        profilePic: true,
+      },
+    });
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "success",
+        result: users,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      },
+    );
+  } catch (err) {
+    console.log("server error: ", err);
+    return new NextResponse(JSON.stringify({ message: "fail" }), {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+}
