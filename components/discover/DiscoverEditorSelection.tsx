f60918e10968191442f99c980faeca42c7184305
tedlee <ted.lee@bclabs.co.kr>
2023-11-19 01:19:40 +0900

    fix: applied react query and initial data

--- components/discover/DiscoverEditorSelection.tsx
+++ components/discover/DiscoverEditorSelection.tsx
@@ -1,13 +1,29 @@
+"use client";
+
 import React from "react";
 import Title from "@/components/common/module/Title";
-import { PlaylistType } from "@/libs/types/common/Song&PlaylistType";
+import { PlaylistType } from "@/libs/types/song&playlistType";
 import FullImageCardContainer from "@/components/common/carousel/full-img/FullImageCardContainer";
+import { useQuery } from "react-query";
+import { getEditorPlaylists } from "@/libs/utils/client/fetchers";
+import {
+  QUERY_CACHE_TIME,
+  QUERY_STALE_TIME,
+} from "@/libs/utils/client/commonValues";
 
 const DiscoverEditorSelection = ({
   editorPlaylists,
 }: {
   editorPlaylists?: PlaylistType[];
 }) => {
+  const { data } = useQuery({
+    queryKey: ["editorPlaylists"],
+    queryFn: () => getEditorPlaylists(),
+    initialData: editorPlaylists,
+    cacheTime: QUERY_CACHE_TIME,
+    staleTime: QUERY_STALE_TIME,
+  });
+
   return (
     <div className={`mb-[260px] xs:mb-[180px]`}>
       <div className={`flex flex-col gap-4 h-full min-h-[200px]`}>
@@ -15,7 +31,7 @@ const DiscoverEditorSelection = ({
           <Title size={`h1`} text={`Featured Playlists`} />
         </div>
         <div className={`mt-12 absolute left-0 bg-white`}>
-          <FullImageCardContainer playlists={editorPlaylists} />
+          <FullImageCardContainer playlists={data} />
         </div>
       </div>
     </div>
