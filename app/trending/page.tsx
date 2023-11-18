8c053aaa03778aff082edd9131d49868e03ffcfe
tedlee <ted.lee@bclabs.co.kr>
2023-11-19 01:21:07 +0900

    fix: applied promise.all & props for initial data

--- app/trending/page.tsx
+++ app/trending/page.tsx
@@ -1,6 +1,34 @@
 import React from "react";
 import TrendingTemplate from "@/components/trending/TrendingTemplate";
+import {
+  getMoodPlaylists,
+  getTrendingCategoriesPlaylists,
+  getTrendingMainPlaylists,
+} from "@/libs/utils/client/fetchers";
+import PlaylistUpdateProvider from "@/components/common/playlist/PlaylistUpdateProvider";
 
-const Page = () => <TrendingTemplate />;
+const Page = async () => {
+  const [mainInitData, categoryInitData, moodInitData] = await Promise.all([
+    getTrendingMainPlaylists(`0`),
+    getTrendingCategoriesPlaylists(`0`),
+    getMoodPlaylists(null, `0`),
+  ]);
+
+  return (
+    <PlaylistUpdateProvider
+      propsData={[
+        ...mainInitData,
+        ...(categoryInitData?.recentMostPlayedCategoryPlaylist || []),
+        ...moodInitData,
+      ]}
+    >
+      <TrendingTemplate
+        mainInitData={mainInitData}
+        categoryInitData={categoryInitData}
+        moodInitData={moodInitData}
+      />
+    </PlaylistUpdateProvider>
+  );
+};
 
 export default Page;
