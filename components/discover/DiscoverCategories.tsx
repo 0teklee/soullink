5c7cf206cae522be2620759183bc075fd42c0ade
tedlee <ted.lee@bclabs.co.kr>
2023-11-19 01:19:38 +0900

    fix: applied react query and initial data

--- components/discover/DiscoverCategories.tsx
+++ components/discover/DiscoverCategories.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import React from "react";
-import PlaylistListContainer from "@/components/common/playlist/playlist-list/PlaylistListContainer";
-import { UserSessionType } from "@/libs/types/common/userType";
+import PlaylistListContainer from "@/components/common/playlist/column-list/PlaylistListContainer";
+import { UserSessionType } from "@/libs/types/userType";
 import { useSession } from "next-auth/react";
 import { useQuery } from "react-query";
 import { getCategoriesPlaylists } from "@/libs/utils/client/fetchers";
 import Title from "@/components/common/module/Title";
 import FiltersList from "@/components/common/playlist/module/FiltersList";
 import { filterCategoryPlaylist } from "@/libs/utils/client/commonUtils";
+import { PlaylistType } from "@/libs/types/song&playlistType";
 
-const DiscoverCategories = () => {
+const DiscoverCategories = ({
+  propsData,
+}: {
+  propsData?: { categoryPlaylists: PlaylistType[]; categories: string[] };
+}) => {
   const { data: session } = useSession() as { data: UserSessionType };
   const { userId } = session || {};
 
@@ -19,6 +24,7 @@ const DiscoverCategories = () => {
   const { data, refetch } = useQuery({
     queryKey: ["categoryPlaylists", userId],
     queryFn: () => getCategoriesPlaylists(userId),
+    initialData: propsData,
   });
 
   const { categoryPlaylists, categories } = data || {
@@ -27,6 +33,11 @@ const DiscoverCategories = () => {
   };
 
   const formattedCategories = ["all", ...categories];
+
+  const selectedCategoryPlaylists = filterCategoryPlaylist(
+    selectedCategory,
+    categoryPlaylists,
+  );
 
   return (
     <div className={`flex flex-col gap-3 items-start`}>
@@ -41,11 +52,7 @@ const DiscoverCategories = () => {
       {!!categoryPlaylists && categoryPlaylists.length > 0 && (
         <PlaylistListContainer
           key={`discover_categories`}
-          playlists={filterCategoryPlaylist(
-            selectedCategory,
-            categoryPlaylists,
-          )}
-          refetch={refetch}
+          playlists={selectedCategoryPlaylists}
         />
       )}
     </div>
