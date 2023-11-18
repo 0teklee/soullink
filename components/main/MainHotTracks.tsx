06c4bbf94aedf573ff7732d72b68ec83cdd8a0ad
tedlee <ted.lee@bclabs.co.kr>
2023-11-19 01:20:14 +0900

    fix: applied react query and initial data (changed to csr)

--- components/main/MainHotTracks.tsx
+++ components/main/MainHotTracks.tsx
@@ -1,25 +1,37 @@
+"use client";
+
 import React from "react";
 import Title from "@/components/common/module/Title";
-import SongTable from "@/components/common/songTable/SongTable";
+import SongTable from "@/components/common/song/table/SongTable";
 import {
   PlaylistType,
   SongType,
   TrendingSongPlaylistType,
-} from "@/libs/types/common/Song&PlaylistType";
+} from "@/libs/types/song&playlistType";
+import { getTrendingSongs } from "@/libs/utils/client/fetchers";
+import { useQuery } from "react-query";
 
 const MainHotTracks = ({
-  trendingsongList,
+  trendingSongList,
 }: {
-  trendingsongList: TrendingSongPlaylistType;
+  trendingSongList: TrendingSongPlaylistType;
 }) => {
+  const { data } = useQuery({
+    queryKey: ["mainPageTrendingSongs"],
+    queryFn: () => getTrendingSongs(),
+    initialData: trendingSongList,
+  });
+
   return (
     <section className={`flex flex-col items-start w-full gap-4`}>
       <Title size={`h1`} text={`Popular Tracks`} />
-      <SongTable
-        songList={trendingsongList.songs as SongType[]}
-        playlist={trendingsongList as PlaylistType}
-        isCreate={false}
-      />
+      {data && data.songs && data.songs.length > 0 && (
+        <SongTable
+          songList={data.songs as SongType[]}
+          playlist={data as PlaylistType}
+          isCreate={false}
+        />
+      )}
     </section>
   );
 };
