e68d09cc37f6507842e8d4697e8ab1347cee6c8a
tedlee <ted.lee@bclabs.co.kr>
2023-11-19 01:28:56 +0900

    fix: applied changed props for initial data for react query

--- components/trending/TrendingTemplate.tsx
+++ components/trending/TrendingTemplate.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import TrendingCategories from "@/components/trending/TrendingCategories";
 import TrendingMood from "@/components/trending/TrendingMood";
 import TrendingMainTrend from "@/components/trending/TrendingMainTrend";
+import { PlaylistType } from "@/libs/types/song&playlistType";
 
-const TrendingTemplate = () => {
+const TrendingTemplate = ({
+  mainInitData,
+  categoryInitData,
+  moodInitData,
+}: {
+  mainInitData: PlaylistType[];
+  categoryInitData: {
+    recentMostPlayedCategoryPlaylist: PlaylistType[];
+    categories: string[];
+  };
+  moodInitData: PlaylistType[];
+}) => {
   return (
     <div className={`flex flex-col items-start gap-12 py-12 xs:py-5`}>
-      <TrendingMainTrend />
-      <TrendingCategories />
-      <TrendingMood />
+      <TrendingMainTrend initData={mainInitData} />
+      <TrendingCategories initData={categoryInitData} />
+      <TrendingMood initData={moodInitData} />
     </div>
   );
 };
