7f5e38ebac61b07a2f0faf35531b2192f9222aef
tedlee <ted.lee@bclabs.co.kr>
2023-11-19 01:27:43 +0900

    fix: applied changed props & initial data, react-query

--- components/user/UserTemplate.tsx
+++ components/user/UserTemplate.tsx
@@ -1,51 +1,88 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { UserSessionType, UserType } from "@/libs/types/common/userType";
-import UserHeader from "@/components/user/module/UserHeader";
+import React from "react";
+import { UserSessionType, UserType } from "@/libs/types/userType";
+import UserHeader from "@/components/user/UserHeader";
 import Title from "@/components/common/module/Title";
 import PlayListSlider from "@/components/common/playlist/PlayListSlider";
 import CommentContainer from "@/components/common/comments/CommentContainer";
 import { useSession } from "next-auth/react";
-import SongTable from "@/components/common/songTable/SongTable";
-import { PlaylistType } from "@/libs/types/common/Song&PlaylistType";
-import { useRouter } from "next/navigation";
+import SongTable from "@/components/common/song/table/SongTable";
+import { PlaylistType } from "@/libs/types/song&playlistType";
+import { useQuery } from "react-query";
+import { getSingleUserProfile } from "@/libs/utils/client/fetchers";
+import PlaylistListContainer from "@/components/common/playlist/column-list/PlaylistListContainer";
+import {
+  QUERY_CACHE_TIME,
+  QUERY_STALE_TIME,
+} from "@/libs/utils/client/commonValues";
 
-const UserTemplate = ({ userProps }: { userProps: UserType }) => {
+const UserTemplate = ({
+  id,
+  userProps,
+}: {
+  id: string;
+  userProps: UserType;
+}) => {
   const { data: session } = useSession() as { data: UserSessionType };
-  const { nickname, createdPlaylists, id: profileId } = userProps;
-  const isPlaylistsEmpty = createdPlaylists?.length === 0;
-  const router = useRouter();
 
-  useEffect(() => {
-    router.refresh();
-  }, [session]);
+  const { data: userData } = useQuery({
+    queryKey: ["user", id],
+    queryFn: () => getSingleUserProfile(id),
+    enabled: !!id,
+    initialData: userProps,
+    cacheTime: QUERY_CACHE_TIME,
+    staleTime: QUERY_STALE_TIME,
+  });
+
+  const {
+    nickname,
+    createdPlaylists,
+    id: profileId,
+    likedSong,
+    likedPlaylists,
+  } = userData || {};
+  const isPlaylistsEmpty = createdPlaylists?.length === 0;
 
   return (
     <section className={`flex flex-col items-center gap-10 pb-10`}>
-      <UserHeader userProfile={userProps} />
+      <UserHeader userProfile={userProps} session={session} />
       <div
         className={`flex flex-col gap-3 items-start w-full text-gray-900 mt-[520px]`}
       >
-        <Title size={`h1`} text={`${nickname}'s Playlists`} />
-        {!isPlaylistsEmpty && <PlayListSlider playLists={createdPlaylists} />}
-        {isPlaylistsEmpty && <Title size={`h2`} text={`No Playlists`} />}
+        <Title size={`h1`} text={`Playlists by ${nickname}`} />
+        {createdPlaylists && !isPlaylistsEmpty && (
+          <PlayListSlider playLists={createdPlaylists} />
+        )}
+        {createdPlaylists && isPlaylistsEmpty && (
+          <Title size={`h2`} text={`No Playlists`} />
+        )}
+      </div>
+      <div className={`flex flex-col gap-3 w-full`}>
+        <Title size={`h2`} text={`Liked Playlists`} />
+        {likedPlaylists && (
+          <PlaylistListContainer playlists={likedPlaylists} isLarge={true} />
+        )}
       </div>
       <div className={`flex flex-col gap-3 w-full`}>
         <Title size={`h2`} text={`Liked Songs`} />
-        <SongTable
-          songList={userProps.likedSong.songs}
-          isCreate={false}
-          playlist={userProps.likedSong as PlaylistType}
-        />
+        {likedSong && (
+          <SongTable
+            songList={likedSong.songs}
+            isCreate={false}
+            playlist={likedSong as PlaylistType}
+          />
+        )}
       </div>
       <div className={`flex flex-col gap-3 items-start w-full text-gray-900`}>
         <Title size={`h2`} text={`Guestbook`} />
-        <CommentContainer
-          postId={profileId}
-          userId={session?.userId}
-          isProfile={true}
-        />
+        {profileId && (
+          <CommentContainer
+            postId={profileId}
+            userId={session?.userId}
+            isProfile={true}
+          />
+        )}
       </div>
     </section>
   );
