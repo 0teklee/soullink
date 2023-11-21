ed56d150e157c5ba87b3834caf1084493b492f25
tedlee <ted.lee@bclabs.co.kr>
2023-11-21 23:58:00 +0900

    fix: applied discover recommend playlist  api

--- components/discover/DiscoverSuggestion.tsx
+++ components/discover/DiscoverSuggestion.tsx
@@ -2,29 +2,32 @@
 
 import React from "react";
 import Title from "@/components/common/module/Title";
-import { useSession } from "next-auth/react";
-import { UserSessionType } from "@/libs/types/userType";
 import { useQuery } from "react-query";
 import { getRecommendedPlaylists } from "@/libs/utils/client/fetchers";
 import PlayListSlider from "@/components/common/playlist/PlayListSlider";
+import { PlaylistType } from "@/libs/types/song&playlistType";
 
-const DiscoverSuggestion = () => {
-  const { data: session } = useSession() as { data: UserSessionType };
-  const { userNickname: nickname, userId } = session || {};
-
-  const { data: recommendedPlaylists } = useQuery({
+const DiscoverSuggestion = ({
+  recommendedPlaylists,
+  userNickname,
+  userId,
+}: {
+  recommendedPlaylists?: PlaylistType[];
+  userNickname?: string;
+  userId?: string;
+}) => {
+  const { data } = useQuery({
     queryKey: ["recommendedPlaylists", userId],
     queryFn: () => getRecommendedPlaylists(userId || ""),
+    initialData: recommendedPlaylists,
   });
 
   return (
     <div className={`flex flex-col gap-4 h-full min-h-[200px]`}>
       <div className={`flex items-center justify-start gap-2`}>
-        <Title size={`h1`} text={`Playlists for ${nickname || "you"}`} />
+        <Title size={`h1`} text={`Playlists for ${userNickname || "you"}`} />
       </div>
-      {recommendedPlaylists && recommendedPlaylists.length > 0 && (
-        <PlayListSlider playLists={recommendedPlaylists} />
-      )}
+      {data && data.length > 0 && <PlayListSlider playLists={data} />}
     </div>
   );
 };
