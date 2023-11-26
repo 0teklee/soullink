45275fc382451d600d3733e22b29eb1ba0e80657
tedlee <ted.lee@bclabs.co.kr>
2023-11-26 18:12:20 +0900

    fix: applied changed react-query v5

--- components/common/module/ReactQueryClientProvider.tsx
+++ components/common/module/ReactQueryClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const ReactQueryClientProvider = ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
