cf8f6cc389630dd9dbb8f1995e8b5583a6d83518
tedlee <ted.lee@bclabs.co.kr>
2023-11-26 17:48:24 +0900

    add: youtube search api added

---
+++ app/api/search/youtube/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { YoutubeSearchResponse } from "@/libs/types/youtubeTypes";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const { search, pageToken } = Object.fromEntries(url.searchParams.entries());
+
+  try {
+    const res = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&pageToken=${
+        pageToken ? pageToken : ""
+      }&q=${search}&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`,
+    );
+    const data: YoutubeSearchResponse | { error: { code: number } } =
+      await res.json();
+
+    if ("error" in data) {
+      return new NextResponse(
+        JSON.stringify({ message: "Youtube API Error" }),
+        {
+          status: 500,
+          statusText: "Youtube API Error",
+        },
+      );
+    }
+
+    return new NextResponse(
+      JSON.stringify({
+        message: "success",
+        result: data,
+      }),
+      {
+        status: 200,
+        statusText: "OK",
+      },
+    );
+  } catch (err) {
+    console.log("server error: ", err);
+    return new NextResponse(JSON.stringify({ message: "fail" }), {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+}
