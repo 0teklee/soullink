8496d38ce72d58ad73f95cb1e23452add74d27db
tedlee <ted.lee@bclabs.co.kr>
2023-12-05 12:09:28 +0900

    fix: applied react-query v5 prefetch and HydraionBoundary

--- components/discover/DiscoverTemplate.tsx
+++ components/discover/DiscoverTemplate.tsx
@@ -1,47 +1,22 @@
 import React from "react";
-import { PlaylistType } from "@/libs/types/song&playlistType";
 import DiscoverMoodLists from "@/components/discover/DiscoverMoodLists";
 import DiscoverSuggestion from "@/components/discover/DiscoverSuggestion";
 import DiscoverEditorSelection from "@/components/discover/DiscoverEditorSelection";
 import DiscoverCategories from "@/components/discover/DiscoverCategories";
 
-interface DiscoverCategoryData {
-  categoryPlaylists: PlaylistType[];
-  categories: string[];
-}
-
 const DiscoverTemplate = ({
-  propsData,
   userId,
   userNickname,
 }: {
-  propsData: (PlaylistType[] | DiscoverCategoryData)[];
   userId?: string;
   userNickname?: string;
 }) => {
-  const [
-    editorPlaylists,
-    moodPlaylists,
-    categoryPlaylists,
-    recommendPlaylists,
-  ] = propsData;
   return (
     <section className={`flex flex-col gap-12 py-6`}>
-      <DiscoverEditorSelection
-        editorPlaylists={editorPlaylists as PlaylistType[]}
-      />
-      <DiscoverMoodLists
-        moodPlaylists={moodPlaylists as PlaylistType[]}
-        userId={userId}
-      />
-      <DiscoverCategories
-        propsData={categoryPlaylists as DiscoverCategoryData}
-      />
-      <DiscoverSuggestion
-        recommendedPlaylists={recommendPlaylists as PlaylistType[]}
-        userNickname={userNickname}
-        userId={userId}
-      />
+      <DiscoverEditorSelection />
+      <DiscoverMoodLists userId={userId} />
+      <DiscoverCategories />
+      <DiscoverSuggestion userNickname={userNickname} userId={userId} />
     </section>
   );
 };
