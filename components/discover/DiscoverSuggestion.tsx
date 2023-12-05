b6cc7ab268d4d823dec68b59135248c50c8b2c5a
tedlee <ted.lee@bclabs.co.kr>
2023-12-05 12:09:45 +0900

    fix: applied react-query v5 prefetch and Hydraion Boundary & Error Boundary

--- components/discover/DiscoverSuggestion.tsx
+++ components/discover/DiscoverSuggestion.tsx
@@ -5,21 +5,18 @@ import Title from "@/components/common/module/Title";
 import { useQuery } from "@tanstack/react-query";
 import { getRecommendedPlaylists } from "@/libs/utils/client/fetchers";
 import PlayListSlider from "@/components/common/playlist/PlayListSlider";
-import { PlaylistType } from "@/libs/types/song&playlistType";
+import ReactQueryErrorBoundary from "@/components/common/react-query-provider/ReactQueryErrorBoundary";
 
 const DiscoverSuggestion = ({
-  recommendedPlaylists,
   userNickname,
   userId,
 }: {
-  recommendedPlaylists?: PlaylistType[];
   userNickname?: string;
   userId?: string;
 }) => {
   const { data } = useQuery({
     queryKey: ["recommendedPlaylists", userId],
     queryFn: () => getRecommendedPlaylists(userId || ""),
-    initialData: recommendedPlaylists,
   });
 
   return (
@@ -27,7 +24,11 @@ const DiscoverSuggestion = ({
       <div className={`flex items-center justify-start gap-2`}>
         <Title size={`h1`} text={`Playlists for ${userNickname || "you"}`} />
       </div>
-      {data && data.length > 0 && <PlayListSlider playlists={data} />}
+      {data && data.length > 0 && (
+        <ReactQueryErrorBoundary>
+          <PlayListSlider playlists={data} />
+        </ReactQueryErrorBoundary>
+      )}
     </div>
   );
 };
