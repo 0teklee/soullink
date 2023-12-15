3f883dbd6fc4d65914429c7706f4a795c56e2967
tedlee <ted.lee@bclabs.co.kr>
2023-12-15 23:01:06 +0900

    fix: applied optimistic update on song like

--- libs/utils/hooks/useMutateSongLike.ts
+++ libs/utils/hooks/useMutateSongLike.ts
@@ -6,6 +6,8 @@ import { useRecoilState } from "recoil";
 import { playlistState } from "@/libs/recoil/atoms";
 import useSetModal from "@/libs/utils/hooks/useSetModal";
 import { MODAL_TYPE } from "@/libs/types/modalType";
+import { Dispatch, SetStateAction } from "react";
+import { formatSongOptimisticSetter } from "@/libs/utils/client/commonUtils";
 
 const useSongLike = () => {
   const queryClient = useQueryClient();
@@ -13,11 +15,36 @@ const useSongLike = () => {
   const [selectedPlaylist, setSelectedPlaylist] = useRecoilState(playlistState);
 
   const { mutate } = useMutation({
-    mutationFn: ({ songId, userId }: { songId: string; userId: string }) => {
+    mutationFn: ({
+      songId,
+      userId,
+    }: {
+      songId: string;
+      userId: string;
+      optimisticSetter?: Dispatch<SetStateAction<boolean>>;
+    }) => {
       if (!userId || !userId) {
         throw new Error("User is not logged in");
       }
       return postSongLike({ songId, userId: userId || "" });
+    },
+    onMutate: ({ songId, userId, optimisticSetter }) => {
+      if (optimisticSetter) {
+        optimisticSetter((prev) => !prev);
+      }
+
+      if (
+        userId &&
+        selectedPlaylist &&
+        selectedPlaylist.songs.filter((song) => song.id === songId).length > 0
+      ) {
+        setSelectedPlaylist((prev) => {
+          if (!prev) {
+            return prev;
+          }
+          return formatSongOptimisticSetter(userId, songId, prev);
+        });
+      }
     },
     onSuccess: async (res) => {
       await queryClient
@@ -47,7 +74,11 @@ const useSongLike = () => {
     },
   });
 
-  const songLikeMutate = (songId?: string, userId?: string) => {
+  const songLikeMutate = (
+    songId?: string,
+    userId?: string,
+    optimisticSetter?: Dispatch<SetStateAction<boolean>>,
+  ) => {
     if (!userId) {
       setLoginModal(MODAL_TYPE.LOGIN);
       return;
@@ -57,7 +88,7 @@ const useSongLike = () => {
       throw new Error("Song id is not provided");
     }
 
-    mutate({ songId, userId });
+    mutate({ songId, userId, optimisticSetter });
   };
 
   return { songLikeMutate };
