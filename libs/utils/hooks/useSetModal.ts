8042fdf8e49e891be7f275ebdb2eae8905139099
tedlee <ted.lee@bclabs.co.kr>
2023-12-17 17:04:04 +0900

    fix: added song table, follow modal cases

--- libs/utils/hooks/useSetModal.ts
+++ libs/utils/hooks/useSetModal.ts
@@ -1,10 +1,12 @@
 import {
   DeleteModalPropsType,
   ErrorModalPropsType,
+  FollowModalPropsType,
   MODAL_TYPE,
   PlaylistEditPropsType,
   ShareDownloadModalPropsType,
   SongModalPropsType,
+  SongTableModalPropsType,
 } from "@/libs/types/modalType";
 import { useRecoilState, useSetRecoilState } from "recoil";
 
@@ -13,19 +15,25 @@ import {
   CommonModalTypeState,
   DeleteModalPropsState,
   ErrorModalPropsState,
+  FollowPropsState,
   PlaylistEditPropsState,
   PlaylistShareDownloadPropsState,
   SongModalPropsState,
+  SongTableModalPropsState,
 } from "@/libs/recoil/modalAtoms";
 
 const useSetModal = () => {
   const setModalOpenState = useSetRecoilState(CommonModalState);
   const setModalTypeState = useSetRecoilState(CommonModalTypeState);
+  const [followModalState, setFollowProps] = useRecoilState(FollowPropsState);
   const [playlistEditModalState, setPlaylistEditProps] = useRecoilState(
     PlaylistEditPropsState,
   );
   const [playlistDownloadModalState, setPlaylistDownloadProps] = useRecoilState(
     PlaylistShareDownloadPropsState,
+  );
+  const [songTableModalState, setSongTableModalProps] = useRecoilState(
+    SongTableModalPropsState,
   );
   const [songCreateEditModalState, setSongModalProps] =
     useRecoilState(SongModalPropsState);
@@ -37,8 +45,12 @@ const useSetModal = () => {
 
   const useModalState = <T>(type: MODAL_TYPE): T => {
     switch (type) {
+      case MODAL_TYPE.FOLLOW:
+        return [followModalState, setFollowProps] as T;
       case MODAL_TYPE.PLAYLIST_EDIT:
         return [playlistEditModalState, setPlaylistEditProps] as T;
+      case MODAL_TYPE.SONG_TABLE:
+        return [songTableModalState, setSongTableModalProps] as T;
       case MODAL_TYPE.SONG:
         return [songCreateEditModalState, setSongModalProps] as T;
       case MODAL_TYPE.DELETE:
@@ -57,6 +69,8 @@ const useSetModal = () => {
     setModalProps?:
       | PlaylistEditPropsType
       | ShareDownloadModalPropsType
+      | FollowModalPropsType
+      | SongTableModalPropsType
       | SongModalPropsType
       | DeleteModalPropsType
       | ErrorModalPropsType,
@@ -69,11 +83,17 @@ const useSetModal = () => {
     }
 
     switch (type_input) {
+      case MODAL_TYPE.FOLLOW:
+        setFollowProps(setModalProps as FollowModalPropsType);
+        break;
       case MODAL_TYPE.PLAYLIST_EDIT:
         setPlaylistEditProps(setModalProps as PlaylistEditPropsType);
         break;
       case MODAL_TYPE.PLAYLIST_DOWNLOAD:
         setPlaylistDownloadProps(setModalProps as ShareDownloadModalPropsType);
+        break;
+      case MODAL_TYPE.SONG_TABLE:
+        setSongTableModalProps(setModalProps as SongTableModalPropsType);
         break;
       case MODAL_TYPE.SONG:
         setSongModalProps(setModalProps as SongModalPropsType);
