048cf781992681eea07857becd389e49d462b442
tedlee <ted.lee@bclabs.co.kr>
2023-12-17 17:49:38 +0900

    fix: full image slider motion logic fixed

--- components/common/carousel/full-img/FullImageCardContainer.tsx
+++ components/common/carousel/full-img/FullImageCardContainer.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react";
 import { PlaylistType } from "@/libs/types/song&playlistType";
 import { motion, useAnimationControls } from "framer-motion";
-import { fillEmptyPlaylist } from "@/libs/utils/client/commonUtils";
 import FullImageCardItem from "@/components/common/carousel/full-img/FullImageCardItem";
 
 const FullImageCardContainer = ({
@@ -13,36 +12,34 @@ const FullImageCardContainer = ({
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const controls = useAnimationControls();
-  const formattedPlaylists = fillEmptyPlaylist(playlists);
-
-  const handleDelay = () => {
-    setCurrentIndex((prev) => {
-      if (prev === formattedPlaylists.length - 1) {
-        return 0;
-      }
-      return prev + 1;
-    });
-  };
 
   useEffect(() => {
     controls.set({ opacity: 0, x: 100 });
     controls.start({
       opacity: 100,
       x: 0,
-      transition: { ease: "easeInOut" },
+      transition: { ease: "easeInOut", duration: 0.6 },
     });
   }, [currentIndex]);
 
   useEffect(() => {
-    setInterval(handleDelay, 1000 * 10);
-  }, []);
+    const interval = setInterval(() => {
+      if (!playlists) {
+        return;
+      }
+      setCurrentIndex((prev) => (prev + 1) % playlists.length);
+    }, 10000);
+    return () => clearInterval(interval);
+  }, [playlists]);
 
   return (
     <div className={`relative w-full h-full `}>
       <div className={` w-full`}>
-        <motion.div animate={controls}>
-          <FullImageCardItem playlist={formattedPlaylists[currentIndex]} />
-        </motion.div>
+        {playlists && (
+          <motion.div animate={controls}>
+            <FullImageCardItem playlist={playlists[currentIndex]} />
+          </motion.div>
+        )}
       </div>
     </div>
   );
