6329b48891b1a76738724b714ac91d515f376ef3
tedlee <ted.lee@bclabs.co.kr>
2023-12-17 17:51:34 +0900

    fix: added case to render section only when the data is available

--- components/main/MainFriendsPlaylists.tsx
+++ components/main/MainFriendsPlaylists.tsx
@@ -15,33 +15,38 @@ const MainFriendsPlaylists = ({ userId }: { userId?: string }) => {
     queryFn: () => getMainPageFriendsPlaylists(userId),
   });
 
+  const dataAvailable = data && data.length > 0;
+
   return (
-    <section className={`flex flex-col items-start w-full gap-4`}>
-      <Title text={`Friends are listening to..`} size={`h1`} />
-      <div className={`flex flex-wrap items-start justify-between gap-3`}>
-        {data &&
-          data.length > 0 &&
-          data.map((item, index) => {
-            return (
-              <div
-                key={`playlist_item_${item.id}_${index}`}
-                className={`relative w-24 h-24 xs:w-16 xs:h-16 hover:bg-black hover:bg-opacity-30 cursor-pointer`}
-              >
-                <Link href={`/playlist/${formatPathName(item.title)}`}>
-                  <Image
-                    className={`object-cover`}
-                    src={
-                      item.coverImage || `/image/common/default_cover_image.svg`
-                    }
-                    alt={`playlist`}
-                    fill={true}
-                  />
-                </Link>
-              </div>
-            );
-          })}
-      </div>
-    </section>
+    <>
+      {dataAvailable && (
+        <section className={`flex flex-col items-start w-full gap-4`}>
+          <Title text={`Friends are listening to..`} size={`h1`} />
+          <div className={`flex flex-wrap items-start justify-between gap-3`}>
+            {data.map((item, index) => {
+              return (
+                <div
+                  key={`playlist_item_${item.id}_${index}`}
+                  className={`relative w-24 h-24 xs:w-16 xs:h-16 hover:bg-black hover:bg-opacity-30 cursor-pointer`}
+                >
+                  <Link href={`/playlist/${formatPathName(item.title)}`}>
+                    <Image
+                      className={`object-cover`}
+                      src={
+                        item.coverImage ||
+                        `/image/common/default_cover_image.svg`
+                      }
+                      alt={`playlist`}
+                      fill={true}
+                    />
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
