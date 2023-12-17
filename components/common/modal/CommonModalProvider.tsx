dd7d3eb1ffcda3da2a5868bf2a9b6d95e72ba421
tedlee <ted.lee@bclabs.co.kr>
2023-12-17 16:56:37 +0900

    add: song table modal added to common modal provider

--- components/common/modal/CommonModalProvider.tsx
+++ components/common/modal/CommonModalProvider.tsx
@@ -13,6 +13,7 @@ import {
   CommonModalTypeState,
   DeleteModalPropsState,
   ErrorModalPropsState,
+  FollowPropsState,
   PlaylistEditPropsState,
   PlaylistShareDownloadPropsState,
   SongModalPropsState,
@@ -20,34 +21,46 @@ import {
 import CommonErrorModal from "@/components/common/modal/CommonErrorModal";
 import ShareDownloadModal from "@/components/playlist/module/share-download-modal/ShareDownloadModal";
 import SearchModal from "@/components/common/search/SearchModal";
+import UserFollowModal from "@/components/user/module/UserFollowModal";
+import CommonSongTableModal from "@/components/common/modal/CommonSongTableModal";
 
 const CommonModalProvider = () => {
   const [isModalOpen, setIsModalOpen] = useRecoilState(CommonModalState);
   const modalTypeState = useRecoilValue(CommonModalTypeState);
+  const setFollowModalProps = useSetRecoilState(FollowPropsState);
   const setPlaylistEditModalProps = useSetRecoilState(PlaylistEditPropsState);
   const setPlaylistShareModalProps = useSetRecoilState(
     PlaylistShareDownloadPropsState,
   );
+  const setSongTableModalProps = useSetRecoilState(SongModalPropsState);
   const setSongModalProps = useSetRecoilState(SongModalPropsState);
   const setDeleteModalProps = useSetRecoilState(DeleteModalPropsState);
   const setErrorModalProps = useSetRecoilState(ErrorModalPropsState);
 
   const isLoginModal = modalTypeState === MODAL_TYPE.LOGIN;
+  const isFollowModal = modalTypeState === MODAL_TYPE.FOLLOW;
+
   const isPlaylistEditModal = modalTypeState === MODAL_TYPE.PLAYLIST_EDIT;
   const isPlaylistShareDownloadModal =
     modalTypeState === MODAL_TYPE.PLAYLIST_DOWNLOAD;
   const isSearchModal = modalTypeState === MODAL_TYPE.SEARCH;
-
+  const isSongTableModal = modalTypeState === MODAL_TYPE.SONG_TABLE;
   const isSongModal = modalTypeState === MODAL_TYPE.SONG;
   const isDeleteModal = modalTypeState === MODAL_TYPE.DELETE;
   const isErrorModal = modalTypeState === MODAL_TYPE.ERROR;
 
   useEffect(() => {
+    if (!isModalOpen && isFollowModal) {
+      setFollowModalProps(null);
+    }
     if (!isModalOpen && isPlaylistEditModal) {
       setPlaylistEditModalProps(null);
     }
     if (!isModalOpen && isPlaylistShareDownloadModal) {
       setPlaylistShareModalProps(null);
+    }
+    if (!isModalOpen && isSongTableModal) {
+      setSongTableModalProps(null);
     }
     if (!isModalOpen && isSongModal) {
       setSongModalProps(null);
@@ -63,9 +76,11 @@ const CommonModalProvider = () => {
   return (
     <CommonModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
       {isLoginModal && <CommonLoginModal />}
+      {isFollowModal && <UserFollowModal />}
       {isPlaylistEditModal && <DetailEditModal />}
       {isPlaylistShareDownloadModal && <ShareDownloadModal />}
       {isSearchModal && <SearchModal />}
+      {isSongTableModal && <CommonSongTableModal />}
       {isSongModal && <PlaylistSongModal />}
       {isDeleteModal && <CommentDeleteModal />}
       {isErrorModal && <CommonErrorModal />}
