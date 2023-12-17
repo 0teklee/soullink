e0e92cef859f794c50f36d3152abf19bc3318e89
tedlee <ted.lee@bclabs.co.kr>
2023-12-17 17:47:47 +0900

    add: added sont table, follow modal types

--- libs/types/modalType.ts
+++ libs/types/modalType.ts
@@ -1,11 +1,17 @@
 import { PlaylistType, SongType } from "@/libs/types/song&playlistType";
 import { SetterOrUpdater } from "recoil";
+import {
+  FollowerOptionNullable,
+  FollowingOptionNullable,
+} from "@/libs/types/userType";
 
 export enum MODAL_TYPE {
   "LOGIN" = "LOGIN",
   "SONG" = "SONG",
+  "SONG_TABLE" = "SONG_TABLE",
   "PLAYLIST_EDIT" = "PLAYLIST",
   "PLAYLIST_DOWNLOAD" = "PLAYLIST_DOWNLOAD",
+  "FOLLOW" = "FOLLOW",
   "SEARCH" = "SEARCH",
   "DELETE" = "DELETE",
   "ERROR" = "ERROR",
@@ -22,6 +28,11 @@ export interface PlaylistEditPropsType {
   addedSongList?: SongType[];
 }
 
+export interface SongTableModalPropsType {
+  userId?: string;
+  songs?: SongType[];
+}
+
 export interface SongModalPropsType {
   modalSong?: SongType;
   isEdit?: boolean;
@@ -29,6 +40,12 @@ export interface SongModalPropsType {
 
 export interface DeleteModalPropsType {
   mutate: () => void;
+}
+
+export interface FollowModalPropsType {
+  follows: FollowerOptionNullable | FollowingOptionNullable;
+  isFollower: boolean;
+  profileNickname: string;
 }
 
 export interface ShareDownloadModalPropsType {
@@ -44,6 +61,10 @@ export interface ShareDownloadModalPropsType {
 }
 
 export interface UseModalStateMap {
+  [MODAL_TYPE.FOLLOW]: [
+    FollowModalPropsType,
+    SetterOrUpdater<FollowModalPropsType | null>,
+  ];
   [MODAL_TYPE.PLAYLIST_EDIT]: [
     PlaylistEditPropsType | null,
     SetterOrUpdater<PlaylistEditPropsType | null>,
@@ -51,6 +72,10 @@ export interface UseModalStateMap {
   [MODAL_TYPE.PLAYLIST_DOWNLOAD]: [
     ShareDownloadModalPropsType | null,
     SetterOrUpdater<ShareDownloadModalPropsType | null>,
+  ];
+  [MODAL_TYPE.SONG_TABLE]: [
+    SongTableModalPropsType | null,
+    SetterOrUpdater<SongTableModalPropsType | null>,
   ];
   [MODAL_TYPE.SONG]: [
     SongModalPropsType | null,
