b5ff801c9cb7b00a2d734af76303cf7e66b564cf
tedlee <ted.lee@bclabs.co.kr>
2023-12-19 23:29:39 +0900

    fix: added duplication validation, fixed to serverside session, style fixed in dark mode

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import SignupTemplate from "@/components/signup/SignupTemplate";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const Page = () => {
-  return <SignupTemplate />;
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+  return <SignupTemplate session={session} />;
 };
 
 export default Page;
