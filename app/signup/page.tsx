6a494f39724a7a806d4111799bce9d602546b4f0
tedlee <ted.lee@bclabs.co.kr>
2023-12-20 10:34:50 +0900

    fix: switch to client side session

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,9 +3,8 @@ import SignupTemplate from "@/components/signup/SignupTemplate";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-const Page = async () => {
-  const session = await getServerSession(authOptions);
-  return <SignupTemplate session={session} />;
+const Page = () => {
+  return <SignupTemplate />;
 };
 
 export default Page;
