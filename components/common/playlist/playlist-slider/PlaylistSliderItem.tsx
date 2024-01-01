12eb541e53b95b275c169b931b6064efec403049
tedlee <ted.lee@bclabs.co.kr>
2024-01-01 16:17:44 +0900

    style: applied sideway scrolls to playlist item

--- components/common/playlist/playlist-slider/PlaylistSliderItem.tsx
+++ components/common/playlist/playlist-slider/PlaylistSliderItem.tsx
@@ -120,16 +120,22 @@ const PlaylistSliderItem = ({
             {songs?.map((song, index) => {
               return (
                 <li
-                  className={`flex items-center justify-center gap-3 w-full px-2 font-semibold md:text-xs xs:justify-between xs:gap-2 xs:overflow-x-hidden`}
+                  className={`grid grid-cols-8 gap-3 w-full px-2 font-semibold md:text-xs xs:justify-between xs:gap-2 xs:overflow-x-hidden`}
                   key={index}
                 >
-                  <span>{index + 1}</span>
-                  <div className={`overflow-x-hidden xs:w-[140px] truncate`}>
-                    <span className={`sideways-scroll`}>{song.title}</span>
+                  <span className={`col-span-1 w-sm`}>{index + 1}</span>
+                  <div
+                    className={`col-span-5 text-start max-w-[140px] overflow-x-hidden`}
+                  >
+                    <div className={`sideways-scroll`}>
+                      <span>{song.title}</span>
+                    </div>
                   </div>
-                  <span>-</span>
-                  <div className={`overflow-x-hidden xs:w-[80px] truncate`}>
-                    <span className={`sideways-scroll`}>{song.artist}</span>
+                  {/*<span>-</span>*/}
+                  <div className={`col-span-2 max-w-[60px] overflow-x-hidden`}>
+                    <div className={`sideways-scroll `}>
+                      <span>{song.artist}</span>
+                    </div>
                   </div>
                 </li>
               );
