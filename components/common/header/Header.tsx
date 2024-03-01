9cffc64509d69a792b7e16b9ba72d8f428f4183b
0teklee <leetekwoo@gmail.com>
2024-03-02 02:43:00 +0900

    style: logo changed

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -3,11 +3,18 @@ import Image from "next/image";
 import HeaderUser from "@/components/common/header/HeaderUser";
 import Link from "next/link";
 import HeaderSearch from "@/components/common/header/HeaderSearch";
+import { NextFont } from "next/dist/compiled/@next/font";
+import { clsx } from "clsx";
 
-const Header = () => {
+const Header = ({ font }: { font: NextFont }) => {
   return (
     <div
-      className={`fixed flex items-center justify-between w-screen px-8 py-3 bg-gray-700 border-b-2 border-primary z-50 xs:py-2 xs:px-2 `}
+      className={clsx(
+        `fixed flex items-center justify-between w-screen px-8 py-3`,
+        `bg-gray-100 text-gray-900 border-b-2 border-primary z-50 `,
+        `dark:text-gray-50 dark:bg-black xs:py-2 xs:px-2 `,
+        font.className,
+      )}
     >
       <div className={`flex items-center w-full h-full gap-6 xs:flex-1`}>
         <Link
@@ -15,43 +22,48 @@ const Header = () => {
           href={`/`}
         >
           <div className={`relative w-8 h-8`}>
-            <Image src={`/soullink_logo.png`} alt="logo" fill={true} />
+            <Image
+              className={`rounded-full dark:invert`}
+              src={`/soullink_logo.png`}
+              alt="logo"
+              fill={true}
+            />
           </div>
           <h1
             className={`text-xl text-primary font-bold md:hidden desktop:box`}
           >
-            Soullink
+            SOUL LINK
           </h1>
         </Link>
         <div
-          className={`flex items-center justify-start gap-4 text-normal text-gray-100  whitespace-nowrap font-normal md:text-sm xs:hidden`}
+          className={`flex items-center justify-start gap-4 text-normal  whitespace-nowrap font-normal md:text-sm xs:hidden`}
         >
           <Link
             href={`/discover`}
-            className={`px-3 py-1 hover:bg-primary hover:text-white rounded-lg`}
+            className={`px-3 py-1 hover:bg-primary hover:text-white dark:hover:text-black rounded-lg`}
           >
             Discover
           </Link>
           <Link
             href={`/trending`}
-            className={`px-3 py-1 hover:bg-primary hover:text-white rounded-lg`}
+            className={`px-3 py-1 hover:bg-primary hover:text-white dark:hover:text-black rounded-lg`}
           >
             Trending
           </Link>
           <Link
             href={`/search`}
-            className={`px-3 py-1 hover:bg-primary hover:text-white rounded-lg`}
+            className={`px-3 py-1 hover:bg-primary hover:text-white dark:hover:text-black rounded-lg`}
           >
             Search
           </Link>
           <Link
-            className={`px-3 py-1 hover:bg-primary hover:text-white rounded-lg`}
+            className={`px-3 py-1 hover:bg-primary hover:text-white dark:hover:text-black rounded-lg`}
             href={`/playlist/create`}
           >
             Create list
           </Link>
           <Link
-            className={`px-3 py-1 text-warmGray-50  hover:text-primary rounded-lg`}
+            className={`px-3 py-1  hover:text-primary rounded-lg`}
             href={`/support`}
           >
             Support
