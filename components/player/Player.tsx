beb1475089f954958365b371daa103a68a4893cd
0teklee <leetekwoo@gmail.com>
2024-03-03 14:59:03 +0900

    refactor: removed unnecessary props, replaced props to custom hook

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -1,64 +1,68 @@
 "use client";
 
-import React, { RefObject, useEffect, useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import {
   formatPlayedSeconds,
   formatSecondsToString,
 } from "@/libs/utils/client/formatter";
 import ReactPlayer from "react-player";
-import { PlayerProps, SongType } from "@/libs/types/song&playlistType";
-import { PlayerType } from "@/libs/types/playerType";
-import { SetterOrUpdater } from "recoil";
-import { handleSourceSet } from "@/components/player/utils";
+import handlePlayerKeyPress, {
+  handleSourceSet,
+} from "@/components/player/utils";
+import usePlayerState from "@/components/player/usePlayerState";
+import dayjs from "dayjs";
+import useSongCountUpdater from "@/libs/utils/hooks/useSongCountUpdater";
 
-const Player = ({
-  song,
-  songList,
-  playerState,
-  setPlayerState,
-  songListIndex,
-  playerRef,
-}: {
-  playerState: PlayerType;
-  setPlayerState: SetterOrUpdater<PlayerType>;
-  song: string;
-  songList: SongType[];
-  songListIndex: number;
-  playerRef: RefObject<PlayerProps>;
-}) => {
+const Player = () => {
+  const {
+    currentSong,
+    song,
+    songId,
+    prevSongId,
+    selectedSongList: songList,
+    playerState,
+    setPlayerState,
+    songListIndex,
+    playerRef,
+  } = usePlayerState();
+
+  const { handleSongChange } = useSongCountUpdater();
+
   const { playing, volume, muted } = playerState;
-  const [browserHiddenProps, setBrowserHiddenProps] = React.useState({
-    muted: false,
-    playing: false,
-  });
 
   const songListSrcset = useMemo(() => {
     return handleSourceSet(songListIndex, songList);
   }, [songListIndex, songList]);
 
-  const handleVisibilityChange = () => {
-    if (document.visibilityState === "hidden" && navigator.maxTouchPoints > 0) {
-      setBrowserHiddenProps((prev) => ({
-        muted: true,
-        playing: true,
-      }));
-      console.log("browser hidden in mobile");
+  useEffect(() => {
+    if (!songId) {
+      return;
+    }
+    if (!prevSongId.current) {
+      prevSongId.current = songId;
     }
 
-    if (document.visibilityState !== "hidden" && navigator.maxTouchPoints > 0) {
-      setBrowserHiddenProps((prev) => ({
-        muted: false,
-        playing: prev.playing,
-      }));
+    if (playerState?.songStartedAt) {
+      handleSongChange(prevSongId.current);
+      prevSongId.current = songId;
     }
-  };
+
+    setPlayerState({
+      ...playerState,
+      songStartedAt: dayjs(new Date()),
+    });
+  }, [currentSong]);
 
   useEffect(() => {
-    document.addEventListener("visibilitychange", handleVisibilityChange);
-    return () => {
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    const handleKeyDown = (e: KeyboardEvent) => {
+      handlePlayerKeyPress(e, playerRef, setPlayerState);
     };
-  }, []);
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setPlayerState, playerRef]);
 
   return (
     <>
@@ -69,9 +73,9 @@ const Player = ({
         autoPlay={true}
         playsinline
         url={song}
-        playing={playing || browserHiddenProps.playing}
+        playing={playing}
         volume={volume}
-        muted={muted || browserHiddenProps.muted}
+        muted={muted}
         onBuffer={() => {
           setPlayerState({
             ...playerState,
